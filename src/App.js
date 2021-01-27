a7a2857395cc62e800bc40ff0daab65730f005de
Bart Dirkson <bhp_dirkson@hhotmail.com>
2021-01-27 11:34:35 +0100

    redux template compleet

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import {increment, decrement} from './actions'
 
 function App() {
   const counter = useSelector (state => state.counter);
-  //const isLogged = useSelector (state => state.isLogged);
+  const isLogged = useSelector (state => state.isLogged);
   const dispatch = useDispatch ();
   
   return (
     <div className="App">
 
-      <h1>Counter {counter} </h1>
+    <h1>Counter {counter} </h1>
       
       <button onClick={() => dispatch(increment (5)) } > + </button>
       <button onClick={() => dispatch(decrement ()) } > - </button>
-      
-
+      {isLogged ?  <h3>Valuable info I shouldn't see</h3> : ''}
+   
 
     </div>
   );
